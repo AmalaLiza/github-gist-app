c12b5b2e03c608a8c183443a5ee328512cad12b2
AmalaLiza <amalaliza555@gmail.com>
2018-12-15 17:25:52 +0530

    banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import connect from 'react-redux/es/connect/connect';
 import styles from '../PublicGists/PublicGists.css';
 import TextField from '../TextField/TextField';
 import { loadPublicGistsOfUser } from '../../actions/action-creator';
-import connect from 'react-redux/es/connect/connect';
 import { selectGists } from '../PublicGists/gists.selector';
+import banner from '../../assets/download.png';
 
 class Home extends Component {
   render() {
     return (
       <div className={styles.searchBoxWrapper}>
+        <img src={banner} />
         <span
-          className={styles.searchHint}>Search users' gist by typing username and hit enter</span>
+          className={styles.searchHint}
+        >
+Search users' gist by typing username and hit enter
+        </span>
         <TextField
           type="text"
           className={styles.searchBox}
